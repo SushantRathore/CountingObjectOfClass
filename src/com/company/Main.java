4b4c01934beea96c0ddbdca2be445f9e3c035520
Sushant-Singh-Rathore <ssr7705@gmail.com>
2020-05-15 16:02:44 +0530

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main  {
+
+    public static void main(String[] args) {
+	// write your code here
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        Object obj3 = new Object();
+
+        obj1.show();
+
+    }
+}
---
+++ src/com/company/Object.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Object {
+
+     static  int i ;
+public  Object()
+{
+    i++;
+}
+    public void show()
+    {
+        System.out.println(i);
+    }
+}
